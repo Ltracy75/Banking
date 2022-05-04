ca58241c98bc7892db84a7c0700d983efe88df11
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-04 16:17:50 -0400

    Add project files.

---
+++ Banking/Accounts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+    {
+    internal class Account
+        {
+        private static int NextId { get; set; } = 1;
+        public int AcctNbr { get; private set; } = 0;
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Balance { get; private set; } = 0;
+
+
+        public bool Deposit(decimal Amount)
+            {
+            if (Amount <= 0)
+                {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+                }
+            Balance = Balance + Amount;
+            return true;
+            }
+        public bool Withdraw(decimal Amount)
+            {
+            if (Amount <= 0)
+                {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+                }
+            if (Amount > Balance)
+                {
+                Console.WriteLine("Insufficient Balance");
+                return false;
+                }
+
+            Balance = Balance - Amount;
+            return true;
+            }
+
+        public bool Transfer(decimal Amount, Account ToAccount)
+            {
+            var success = Withdraw(Amount);
+            if (success)
+                {
+                ToAccount.Deposit(Amount);
+                return true;
+                }
+            return false;
+            }
+        public Account() 
+            {
+            AcctNbr = NextId++;
+            }
+        }
+    }
---
+++ Banking/Program.cs
@@ -0,0 +1,33 @@
+﻿using Banking;
+
+var Act1 = new Account()
+    {
+    Name = "Checking 1",
+    };
+
+var Act2 = new Account()
+    {
+    Name = "Checking 2",
+    };
+
+Act1.Deposit(44000);
+Act2.Deposit(500);
+
+
+Act1.Withdraw(200);
+Act2.Withdraw(400);
+
+Console.WriteLine($"Act1 Balance is {Act1.Balance:c} \nAct2 Balance is {Act2.Balance:c}");
+
+Act2.Withdraw(400);
+
+Console.WriteLine($"Act1 Balance is {Act1.Balance:c} \nAct2 Balance is {Act2.Balance:c}");
+
+Act2.Transfer(100, Act1);
+
+Console.WriteLine($"Act1 Balance is {Act1.Balance:c} \nAct2 Balance is {Act2.Balance:c}");
+
+Act1.Deposit(-200);
+Act2.Withdraw(-200);
+
+Console.WriteLine($"Act1 Balance is {Act1.Balance:c} \nAct2 Balance is {Act2.Balance:c}");
